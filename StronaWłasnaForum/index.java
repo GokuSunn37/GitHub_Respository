3f88f295c81df07fcb17acf89850585c81586149
GokuSunn37 <gokusunn37@protonmail.com>
2022-10-26 12:58:41 +0200

    Uzupełnienie

---
+++ StronaWłasnaForum/index.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class Employee {
+    private String name = "";
+    private int salary = 0;
+    private LocalDate date;
+
+    public Employee(String name, int salary, LocalDate date) {
+        String name = "";
+        int salary = 0;
+    }
+
+
+}
